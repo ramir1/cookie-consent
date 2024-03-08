109ea232d534cda147f0c0a6e5254487cb145f41
Vadim Trofimov <vadimtrofimov@gmail.com>
2024-03-08 11:52:24 +0100

    Added: translates

---
+++ lang/es/banner.php
@@ -0,0 +1,13 @@
+<?php
+return[
+    'first_block'=>'Utilizamos cookies y otras tecnologías de seguimiento para mejorar su experiencia de navegación en nuestro sitio web, mostrarle contenido personalizado y anuncios dirigidos, analizar el tráfico de nuestro sitio web y comprender de dónde provienen nuestros visitantes.',
+    'ok'=>'Ok',
+    'setting'=>'Melodía',
+    'necessary_desc'=>'<b>Necesario</b>: Sin estas cookies, no podemos ofrecerle ciertas funciones en nuestro sitio web.',
+    'preferences'=>'<b>Funcional</b>: almacena las preferencias del usuario, como el idioma, la configuración de la interfaz y la región, para mejorar la experiencia de uso del sitio web.',
+    'statistics'=>'<b>Analítico</b>: ayuda a analizar cómo utiliza nuestro sitio para que podamos mejorar su funcionalidad y contenido.',
+    'marketing'=>'<b>Publicidad</b>: Se utiliza para mostrarle anuncios que pueden ser más relevantes para sus intereses.',
+    'back'=>'Atrás',
+    'apply'=>'Aceptar',
+    'allow_all'=>'Acepta todos'
+];
