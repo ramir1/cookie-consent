4178b21c589bb42551562c6e5ed6e6190ed4fe2e
Vadim Trofimov <vadimtrofimov@gmail.com>
2024-04-11 16:17:50 +0200

    Changed: pint refactoring

--- src/CookieConsentServiceProvider.php
+++ src/CookieConsentServiceProvider.php
@@ -1,23 +1,24 @@
-<?php namespace Ramir1\CookieConsent;
+<?php
+
+namespace Ramir1\CookieConsent;
 
 use Illuminate\Support\ServiceProvider;
 
 class CookieConsentServiceProvider extends ServiceProvider
 {
-
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'cookie_consent');
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'cookie_consent');
-        $this->loadJSONTranslationsFrom(__DIR__ . '/../lang');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'cookie_consent');
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'cookie_consent');
+        $this->loadJSONTranslationsFrom(__DIR__.'/../lang');
 
         $this->publishes([
-            __DIR__ . '/../public/cookie-consent.css' => public_path('vendor/cookie-consent/cookie-consent.css'),
-            __DIR__ . '/../public/cookie-consent.css.gz' => public_path('vendor/cookie-consent/cookie-consent.css.gz')
+            __DIR__.'/../public/cookie-consent.css' => public_path('vendor/cookie-consent/cookie-consent.css'),
+            __DIR__.'/../public/cookie-consent.css.gz' => public_path('vendor/cookie-consent/cookie-consent.css.gz'),
         ], ['assets', 'cookie-consent']);
         $this->publishes([
-            __DIR__ . '/../public/cookie-consent.js' => public_path('vendor/cookie-consent/cookie-consent.js'),
-            __DIR__ . '/../public/cookie-consent.js.gz' => public_path('vendor/cookie-consent/cookie-consent.js.gz')
+            __DIR__.'/../public/cookie-consent.js' => public_path('vendor/cookie-consent/cookie-consent.js'),
+            __DIR__.'/../public/cookie-consent.js.gz' => public_path('vendor/cookie-consent/cookie-consent.js.gz'),
 
         ],
             ['assets', 'cookie-consent']);
@@ -25,8 +26,6 @@ class CookieConsentServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-//        $this->app->register(CookieConsentServiceProvider::class);
+        //        $this->app->register(CookieConsentServiceProvider::class);
     }
-
-
 }
