adc46ec7d76c9d3209093875f5cc77bcd06841f2
Vadim Trofimov <vadimtrofimov@gmail.com>
2024-04-11 16:17:57 +0200

    Changed: pint refactoring

--- lang/en/banner.php
+++ lang/en/banner.php
@@ -1,13 +1,14 @@
 <?php
-return[
-    'first_block'=>'We use cookies and other tracking technologies to improve your browsing experience on our website, to show you personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors are coming from.',
-    'ok'=>'Ok',
-    'setting'=>'Configure',
-    'necessary_desc'=>'<b>Necessary</b>: Without these cookies we cannot provide you with certain features on our website.',
-    'preferences'=>' <b>Functional</b> : Stores user preferences such as language, interface settings and region to improve the experience of using the website.',
-    'statistics'=>'<b>Analytical</b>: helps analyze how you use our site so we can improve its functionality and content.',
-    'marketing'=>'<b>Advertising</b>: used to show you advertisements that may be more relevant to your interests.',
-    'back'=>'Back',
-    'apply'=>'Accept',
-    'allow_all'=>'Accept all'
+
+return [
+    'first_block' => 'We use cookies and other tracking technologies to improve your browsing experience on our website, to show you personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors are coming from.',
+    'ok' => 'Ok',
+    'setting' => 'Configure',
+    'necessary_desc' => '<b>Necessary</b>: Without these cookies we cannot provide you with certain features on our website.',
+    'preferences' => ' <b>Functional</b> : Stores user preferences such as language, interface settings and region to improve the experience of using the website.',
+    'statistics' => '<b>Analytical</b>: helps analyze how you use our site so we can improve its functionality and content.',
+    'marketing' => '<b>Advertising</b>: used to show you advertisements that may be more relevant to your interests.',
+    'back' => 'Back',
+    'apply' => 'Accept',
+    'allow_all' => 'Accept all',
 ];
